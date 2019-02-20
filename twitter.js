c745e2361c882335723ec4fa386ecc807eba0f72
tmcamillo <imeio.tmc@gmail.com>
2019-02-20 16:18:22 -0300

    versão 0.0.3

--- twitter.js
+++ twitter.js
@@ -1,52 +1,59 @@
 
+let filledInput = document.getElementById('sentence');
+let btn = document.getElementById("btn-tweet");
+let counter = document.getElementById("count-down");
+const sentence = document.getElementById("sentence");
+const maxCharacter = 140;
+
 document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('btn-tweet').disabled = "true";
     document.getElementById("btn-tweet").style.opacity = "0.5";
 });
 
-
-var filledInput = document.getElementById('sentence');
-
 filledInput.addEventListener("keyup", function() {
-    let sentenceInput = document.getElementById("sentence").value;
-    if (sentenceInput === "") {
+    if (sentence.value === "") {
         document.getElementById("btn-tweet").disabled = true;
         document.getElementById("btn-tweet").style.opacity = "0.5";
     }
+
     else {
         document.getElementById('btn-tweet').disabled = false;
         document.getElementById("btn-tweet").style.opacity = "1";
     }
 });
 
-var btn = document.getElementById("btn-tweet");
-
 btn.addEventListener("click", function(event){
     event.preventDefault() 
 });
 
 btn.addEventListener("click",function(){
-    let sentence = document.getElementById("sentence").value;
-    document.getElementById("tweet").innerHTML = sentence;
+    document.getElementById("tweet").innerHTML = sentence.value;
 });
 
-function countDown() {
-    let sentence = document.getElementById("sentence").value;
-    let character = sentence.length;
-    let maxCharacter = 140;
+sentence.addEventListener("keyup", function(){
+    let character = sentence.value.length;
     let total = maxCharacter - character;
     document.getElementById("count-down").innerHTML = total;
-};
+});
 
-
-
-/*
-    if (total === 10) {
-        message.style.color = 'rgb(255, 0, 0)';
-    };
-
+sentence.addEventListener("keyup", function(){
+    let character = sentence.value.length;
+    let total = maxCharacter - character;
+    document.getElementById("count-down").innerHTML = total;
+    
     if (total === 20) {
-        message.style.color = 'rgb(255, 153, 0)';
+        document.getElementById("count-down").style.color = 'green';
     }
-*/
+
+    else if (total === 10) {
+        document.getElementById("count-down").style.color = 'red';
+    }
+
+    else if (total > maxCharacter) {
+        document.getElementById("btn-tweet").disabled = true;
+        document.getElementById("btn-tweet").style.opacity = "0.5";
+    }
+
+});
+
 

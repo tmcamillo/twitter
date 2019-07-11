d52a536e047e745ef2aa7954709f01e32b13a53f
tmcamillo <imeio.tmc@gmail.com>
2019-07-11 12:13:35 -0300

    refatorado variaveis e corrigido ação para resetar cor no countdown qd tweet é postado

--- twitter.js
+++ twitter.js
@@ -1,74 +1,79 @@
-
-let filledInput = document.getElementById('sentence');
-let btn = document.getElementById("btn-tweet");
-let counter = document.getElementById("count-down");
-var textarea = document.querySelector("textarea");
+const filledInput = document.getElementById("sentence");
+const btnTweet = document.getElementById("btn-tweet");
+const counter = document.getElementById("count-down");
+const textarea = document.querySelector("textarea");
 const sentence = document.getElementById("sentence");
+const tweetPosted = document.getElementById("tweet");
+const countDown = document.getElementById("count-down");
 const maxCharacter = 140;
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById('btn-tweet').disabled = "true";
-    document.getElementById("btn-tweet").style.opacity = "0.5";
+  btnTweet.disabled = "true";
+  btnTweet.style.opacity = "0.5";
 });
 
 filledInput.addEventListener("keyup", function() {
-    if (sentence.value.length > maxCharacter) {
-        document.getElementById("btn-tweet").disabled = true;
-        document.getElementById("btn-tweet").style.opacity = "0.5";
-    }
-
-    else {
-        document.getElementById('btn-tweet').disabled = false;
-        document.getElementById("btn-tweet").style.opacity = "1";
-    }
+  if (sentence.value.length > maxCharacter) {
+    btnTweet.disabled = true;
+    btnTweet.style.opacity = "0.5";
+  } else {
+    btnTweet.disabled = false;
+    btnTweet.style.opacity = "1";
+  }
 });
 
-btn.addEventListener("click", function(event){
-    event.preventDefault();
+btnTweet.addEventListener("click", function(event) {
+  event.preventDefault();
+  tweetPosted.innerHTML = sentence.value;
+  sentence.value = "";
+  btnTweet.disabled = true;
+  btnTweet.style.opacity = "0.5";
+  countDown.innerHTML = maxCharacter;
+  countDown.removeAttribute("style");
+  timePosted();
 });
+/*
+btn.addEventListener("click",function() {
+    let newdiv = document.createElement('div');
+    let newtime = document.createElement('p');
+    let newtweet = document.createElement('p');
+    newtweet.innerHTML = sentence.value;
+    newtime.innerHTML = timePosted();
 
-btn.addEventListener("click",function(){
-    document.getElementById("tweet").innerHTML = sentence.value;
-    document.getElementById("sentence").value = '';
-    document.getElementById("btn-tweet").disabled = true;
-    document.getElementById("btn-tweet").style.opacity = "0.5";
-    timePosted();
+    newdiv.appendChild(newtweet);
+    newdiv.appendChild(newtime);
+
+    document.getElementById("tweet").appendChild(newdiv);
 });
+*/
 
+sentence.addEventListener("keyup", function() {
+  let character = sentence.value.length;
+  let total = maxCharacter - character;
+  countDown.innerHTML = total;
 
-sentence.addEventListener("keyup", function(){
-    let character = sentence.value.length;
-    let total = maxCharacter - character;
-    document.getElementById("count-down").innerHTML = total;
-    
-    if (total >= 11 && total <= 20) {
-        document.getElementById("count-down").style.color = 'green';
-    }
-
-    else if (total >= 0 && total <= 10) {
-        document.getElementById("count-down").style.color = 'red';
-    }
+  if (total >= 11 && total <= 20) {
+    countDown.style.color = "green";
+  } else if (total >= 0 && total <= 10) {
+    countDown.style.color = "red";
+  } else {
+    countDown.removeAttribute("style");
+  }
 });
 
 // cssText: returns or sets the text of the element's inline style declaration only.
-textarea.addEventListener('keydown', function(){
-    textarea.style.cssText = 'height:auto; padding:0';
-    textarea.style.cssText = 'height:' + textarea.scrollHeight + 'px';
+textarea.addEventListener("keydown", function() {
+  textarea.style.cssText = "height:auto; padding:0";
+  textarea.style.cssText = "height:" + textarea.scrollHeight + "px";
 });
 
-//operador ternário - atalho da condição if - se condition é true, o operador retornará o valor de expr1; se não, ele retorna o valor de exp2. 
 function timePosted() {
-    let now = new Date();
-    let hours = now.getHours().toString();
-    let minutes = (now.getMinutes().toString() < 10 ? '0':'') + now.getMinutes().toString();
-    let timeNow = hours + ':' + minutes;
-    
-    document.getElementById("time").innerHTML = timeNow;
-};
+  let now = new Date();
+  let hours = now.getHours().toString();
+  let minutes =
+    (now.getMinutes().toString() < 10 ? "0" : "") + now.getMinutes().toString();
+  let timeNow = hours + ":" + minutes;
 
-
-
-
-
-
-
+  //   document.getElementById("time").innerHTML = timeNow;
+  return timeNow;
+}
